2312a80493b60087e602d4dc9e7bec9f530dd58e
Easternday <849919718@qq.com>
2019-12-11 16:11:55 +0800

    I like DFS

--- Graph/Martix/AdjacencyMatrix.h
+++ Graph/Martix/AdjacencyMatrix.h
@@ -2,14 +2,8 @@
 #include <iostream>
 using namespace std;
 
-#define MAXVERTEX 100  //图的最大顶点数
-#define INFINITY 32767 //用有符号的int最大值表示无穷大
-
-enum GraphType
-{
-    Diagraph,
-    Graph
-};
+#define MAXVERTEX 100                             //图的最大顶点数
+#define INFINITY (1 << (sizeof(int) * 8 - 1) - 1) //用有符号的int最大值表示无穷大
 
 typedef char vertextype; //定义定点的存储信息为字符型
 typedef int arctype;     //定义边的权值为int型
@@ -17,55 +11,119 @@ typedef int arctype;     //定义边的权值为int型
 //图的邻接矩阵的存储结构
 typedef struct
 {
-    vertextype vertex[MAXVERTEX];      //顶点表
-    arctype arc[MAXVERTEX][MAXVERTEX]; //邻接矩阵
-    int vertexnum;                     //图的当前顶点数
-    int arcnum;                        //图的当前边数
-    GraphType GT;                      //图类型
-
+    vertextype Vertex[MAXVERTEX];      //顶点表
+    arctype Arc[MAXVERTEX][MAXVERTEX]; //邻接矩阵
+    int Vertexnum;                     //图的当前顶点数
+    int Arcnum;                        //图的当前边数
 } MGraph;
 
 //创建无向网
 void CreateMGraph(MGraph &G)
 {
+    //输入顶点数目
     cout << "输入顶点数目:";
-    cin >> G.vertexnum; //输入顶点数目
-    cout << "输入边数:";
-    cin >> G.arcnum; //输入边数
-    cout << "输入顶点标号:";
-    for (int i = 0; i < G.vertexnum; i++) //输入顶点信息
+    cin >> G.Vertexnum;
+
+    //合法性检查
+    if (G.Vertexnum > MAXVERTEX)
     {
-        cin >> G.vertex[i];
+        cout << "节点数目太多，不方便给你整。" << endl;
+        exit(0);
     }
-    for (int i = 0; i < G.vertexnum; i++) //将所有边初始化为无穷大
-        for (int j = 0; j < G.vertexnum; j++)
-            G.arc[i][j] = INFINITY;
-    for (int k = 0; k < G.arcnum; k++)
+    if (G.Vertexnum <= 0)
     {
-        int i, j, w;
-        cin >> i >> j; //输入构成边的两个顶点
-        cin >> w;      //输入边所对应的权值
-        G.arc[i][j] = w;
-        G.arc[j][i] = G.arc[i][j]; //无向图的邻接矩阵为对称矩阵
+        cout << "节点数目不太对，不给你整。" << endl;
+        exit(0);
+    }
+
+    G.Arcnum = 0;
+
+    //输入顶点信息
+    cout << "输入顶点标号（除#外一切符号）:";
+    for (int i = 0; i < G.Vertexnum; i++)
+    {
+        cin >> G.Vertex[i];
+    }
+
+    //表格初始化
+    for (int i = 0; i < G.Vertexnum; i++) //将所有边初始化为无穷大
+    {
+        for (int j = 0; j < G.Vertexnum; j++)
+        {
+            G.Arc[i][j] = INFINITY; //值最大化
+        }
+    }
+
+    cout << "输入边信息：" << endl;
+    cout << "起始点 结束点 权值" << endl;
+    //连接边
+    char flag;
+    int i, j, w;
+    while (true)
+    {
+        cin >> flag;
+        if (flag == '#')
+        {
+            return;
+        }
+        cin >> i >> j >> w;
+        G.Arc[i][j] = w; //赋权
+        G.Arcnum++;      //边数增加
     }
 }
 
 //打印邻接矩阵
 void PrintfMGraph(MGraph G)
 {
-    for (int i = 0; i < G.vertexnum; i++)
+    cout << '\t';
+    for (int i = 0; i < G.Vertexnum; i++)
     {
-        for (int j = 0; j < G.vertexnum; j++)
-            cout << G.arc[i][j] << '\t';
+        cout << G.Vertex[i] << '\t';
+    }
+    cout << endl;
+    for (int i = 0; i < G.Vertexnum; i++)
+    {
+        cout << G.Vertex[i] << '\t';
+        for (int j = 0; j < G.Vertexnum; j++)
+        {
+            if (G.Arc[i][j] == INFINITY)
+            {
+                cout << '#' << '\t';
+            }
+            else
+            {
+
+                cout << G.Arc[i][j] << '\t';
+            }
+        }
         cout << endl;
     }
 }
 
-//主函数
-int main()
+//深度搜索(递归)
+void DFS_Rec(MGraph G, int i, bool *Visited)
 {
-    MGraph G;
-    CreateMGraph(G);
-    PrintfMGraph(G);
-    return 0;
+    Visited[i] = true;   //设置为遍历过
+    cout << G.Vertex[i]; //输出
+    for (int j = 0; j < G.Vertexnum; j++)
+    {
+        if (G.Arc[i][j] != INFINITY && !Visited[j])
+        {
+            DFS_Rec(G, j, Visited);
+        }
+    }
+}
+
+//深度遍历最小生成树
+void DFSTraveser(MGraph G)
+{
+    bool *Visited = new bool[G.Vertexnum];
+    for (int i = 0; i < G.Vertexnum; i++)
+    {
+        if (!Visited[i])
+        {
+            DFS_Rec(G, i, Visited);
+            cout << endl;
+        }
+    }
 }
--- Graph/Martix/main.cpp
+++ Graph/Martix/main.cpp
@@ -1,7 +1,24 @@
 #include "AdjacencyMatrix.h"
 
+MGraph G;
+
+/**
+ * 5个结点
+ * ABCDE
+ * @ 0 1 5
+ * @ 1 2 6
+ * @ 1 3 8
+ * @ 2 3 7
+ * @ 0 4 9
+ * @ 3 4 10
+ * #
+*/
+
 int main()
 {
+    CreateMGraph(G); //初始化
+    PrintfMGraph(G); //打印
 
+    DFSTraveser(G);
     return 0;
 }
